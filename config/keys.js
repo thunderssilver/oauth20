9f09bab6ba9559a764129d416b9384b84ab10c23
CV Rohit <51963891+rohitchatla@users.noreply.github.com>
2020-05-03 12:37:19 +0530

    Create keys.js

---
+++ config/keys.js
@@ -0,0 +1,19 @@
+// add this file to .gitignore for git
+
+module.exports = {
+        google: {
+            clientID: '',
+            clientSecret: ''
+        },
+        mongodb: {
+            dbURI: ''
+        },
+        session: {
+            cookieKey: '<some secret code>'
+        },
+        facebookAuth : {
+        'clientID': '',
+        'clientSecret': '',
+        'callbackURL': ''
+    },
+};
